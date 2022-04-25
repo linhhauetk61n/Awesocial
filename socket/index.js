aa45da01fb45409e069c88e25f6423ad7ac9d15e
linhhauetk61n <ngoclinh.windy@gmail.com>
2022-04-25 16:27:21 +0700

    Deploying socket server fix url client

--- socket/index.js
+++ socket/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://awesocial-reactapp.netlify.app/",
     },
 });
 
